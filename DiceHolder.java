3b91f53cc40c2a7f0b3cd9659b93b299597282a7
Escherwes <45395167+Escherwes@users.noreply.github.com>
2018-12-05 09:06:46 -0500

    Initial Attempt
    Fully tried without Partner A code

--- DiceHolder.java
+++ DiceHolder.java
@@ -1,3 +1,41 @@
+import java.util.ArrayList;
 public class DiceHolder{
+
+	Die die = new Die();
+
+	private ArrayList<Die> list;
+
+	public DiceHolder(){
+
+		list = new ArrayList<>();
+
+	}
+
+	public int addDie(Die die){
+
+		if(list.size()<6){
+			list.add(die);
+			return 1;
+		}
+
+		else
+			return -1;
+
+	}
+
+	public void shake(){
+
+		for(int i = 0; i<list.size(); i++){
+			int rand = (int)(Math.random()*list.size())+1;
+			list.add(list.remove(rand));
+		}
+
+	}
+
+	public String toString(){
+
+		return die+"\n";
+
+	}
 
 }
